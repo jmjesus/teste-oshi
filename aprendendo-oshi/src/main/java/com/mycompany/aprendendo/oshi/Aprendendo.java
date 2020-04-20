e4ca3fb3567ad5a46f76e572204b771a3a1cb4fd
João Marcos <joao.jesus@bandtec.com.br>
2020-04-20 15:01:51 -0300

    estudando metodos p/ obter informacoes do sistema operacional, da memoria e do processador

--- aprendendo-oshi/src/main/java/com/mycompany/aprendendo/oshi/Aprendendo.java
+++ aprendendo-oshi/src/main/java/com/mycompany/aprendendo/oshi/Aprendendo.java
@@ -21,6 +21,7 @@ public class Aprendendo {
         HardwareAbstractionLayer hal = si.getHardware();
         CentralProcessor cpu = hal.getProcessor();
         OperatingSystem os = si.getOperatingSystem();
+        GlobalMemory memory = hal.getMemory();
         
         //RETORNA A MEMORIA EM USO ATUAL: Available: 692,9 MiB/3,9 GiB
         //System.out.println(hal.getMemory());
@@ -60,6 +61,54 @@ public class Aprendendo {
         //System.out.println(SystemInfo.getCurrentPlatformEnum());
         
         //RETORNA A MEMORIA DISPONIVEL EM BYTES
-        System.out.println(hal.getMemory().getAvailable());
+        //System.out.println(hal.getMemory().getAvailable());
+
+        // -------------------------------------------------------------------------
+
+        System.out.println(os.toString());
+        // Microsoft Windows 10 (Home) build 18363
+        
+        System.out.println(String.format("Booted: %s", Instant.ofEpochSecond(os.getSystemBootTime())));
+        // Booted: 2020-04-19T16:08:41Z - DATA VEIO NO FORMATO CERTO MAS ESTAVA ERRADA
+        
+        System.out.println(String.format("Uptime: %s", FormatUtil.formatElapsedSecs(os.getSystemUptime())));
+        // Uptime: 0 days, 21:46:18 - formato x dias, Hora:Minutos:Segundos - MAS RETORNOU O TEMPO ERRADO
+     
+        System.out.println(String.format("Running with%s", (os.isElevated() ? "" : "out") + " elevated permissions."));
+        // Running without elevated permissions. - APARENTEMENTE DEU CERTO
+        
+        //private static void printComputerSystem(final ComputerSystem computerSystem) {
+        //oshi.add("system: " + computerSystem.toString());
+        //oshi.add(" firmware: " + computerSystem.getFirmware().toString());
+        //oshi.add(" baseboard: " + computerSystem.getBaseboard().toString());
+        //} 
+        //PRECISO APRENDER MELHOR COMO FUNCIONA
+        
+        System.out.println(String.format("%s", hal.getProcessor()));
+        //Intel(R) Core(TM) i3-5015U CPU @ 2.10GHz
+        //1 physical CPU package(s)
+        //2 physical CPU core(s)
+        //4 logical CPU(s)
+        //Identifier: Intel64 Family 6 Model 61 Stepping 4
+        //ProcessorID: BFEBFBFF000306D4
+        //Microarchitecture: Broadwell (Client)
+
+        System.out.println(String.format("Memory: %s", hal.getMemory()));
+        //Available: 583,1 MiB/3,9 GiB
+        
+        System.out.println(String.format("Swap: %s", memory.getVirtualMemory()));
+        //Used: 613,3 MiB/3,2 GiB
+
+        PhysicalMemory[] pmArray = memory.getPhysicalMemory();
+
+        if (pmArray.length > 0) {
+            System.out.println("Physical Memory: ");
+            for (PhysicalMemory pm : pmArray) {
+                System.out.println(" " + pm.toString());
+            }
+        }
+        //Physical Memory: 
+        //Bank label: BANK 0, Capacity: 4 GiB, Clock speed: 1,6 GHz, Manufacturer: 0715, Memory type: DDR3
+        //System.out.println(String.format("%s", memory.getPhysicalMemory().toString())); - TENTEI FAZER O MESMO COM ESSE COMANDO MAS N DEU CERTO
     }
 }
